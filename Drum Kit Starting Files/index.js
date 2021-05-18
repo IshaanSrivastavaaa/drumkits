586e24f65d110aa26a9ecb4907b572205f554a6c
IshaanSrivastavaaa <48021627+IshaanSrivastavaaa@users.noreply.github.com>
2021-05-18 17:23:29 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,70 @@
+numberOfButtons = document.querySelectorAll(".drum").length;
+for(var i=0;i<numberOfButtons;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var buttonInnerHTML=this.innerHTML;
+        if(buttonInnerHTML=="w"){
+            var audio=new Audio("sounds/tom-1.mp3");
+            audio.play(); 
+        }
+        else if(buttonInnerHTML=="a"){
+            var audio=new Audio("sounds/tom-2.mp3");
+            audio.play();   
+            
+        }else if(buttonInnerHTML=="s"){
+            var audio=new Audio("sounds/tom-3.mp3");
+            audio.play(); 
+            
+        }else if(buttonInnerHTML=="d"){
+            var audio=new Audio("sounds/tom-4.mp3");
+            audio.play(); 
+            
+        }else if(buttonInnerHTML=="j"){
+            var audio=new Audio("sounds/crash.mp3");
+            audio.play(); 
+            
+        }else if(buttonInnerHTML=="k"){
+            var audio=new Audio("sounds/kick-bass.mp3");
+            audio.play(); 
+            
+        }else if(buttonInnerHTML=="l"){
+            var audio=new Audio("sounds/snare.mp3");
+            audio.play(); 
+            
+        }
+})};
+
+document.querySelectorAll(addEventListener("keypress",function(event){
+        makesound(event.key);
+    }))
+
+        function makesound(key){
+            if(key=="w"){
+                var audio=new Audio("sounds/tom-1.mp3");
+                audio.play(); 
+            }
+            else if(key=="a"){
+                var audio=new Audio("sounds/tom-2.mp3");
+                audio.play();   
+                
+            }else if(key=="s"){
+                var audio=new Audio("sounds/tom-3.mp3");
+                audio.play(); 
+                
+            }else if(key=="d"){
+                var audio=new Audio("sounds/tom-4.mp3");
+                audio.play(); 
+                
+            }else if(key=="j"){
+                var audio=new Audio("sounds/crash.mp3");
+                audio.play(); 
+                
+            }else if(key=="k"){
+                var audio=new Audio("sounds/kick-bass.mp3");
+                audio.play(); 
+                
+            }else if(key=="l"){
+                var audio=new Audio("sounds/snare.mp3");
+                audio.play(); 
+                
+            }
+        }
